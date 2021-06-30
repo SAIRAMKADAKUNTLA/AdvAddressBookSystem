9948d139260fc053fa8a10754fa55bceb60359d0
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-07-01 00:02:57 +0530

    [Sairam]Added:Ability to create table in the database

--- AdvBook/Book.sql
+++ AdvBook/Book.sql
@@ -1,1 +1,13 @@
 create database AddressBookService;
+use AddressBookService;
+create table Address_Book
+(
+first_name varchar(255),
+last_name varchar(255),
+address varchar(255),
+city varchar(255),
+state varchar(255),
+zipcode int,
+phone_number varchar(255),
+email_id varchar(255),
+);
