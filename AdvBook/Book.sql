6f8001d1ecdb6645721bd90fa06471c299ce4eee
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-07-01 15:10:30 +0530

    [Sairam]Added:Ability to delete the contacts into address book

--- AdvBook/Book.sql
+++ AdvBook/Book.sql
@@ -19,3 +19,4 @@ values('Ranjith','Kumar','Nehuru street','Mumbai','Maharastra','400768','7546637
 ('Martin','Guptill','hij Street','Indore','Punjab','505536','7644564568','martin@gmail.com')
 select * from Address_Book
 update Address_Book set first_name='Raj'where first_name='Ranjith';
+delete from Address_Book where first_name='Raj';
