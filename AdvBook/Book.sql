d4dd5815a5d4a50cca970ce3614888aa17ab9b71
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-07-01 15:00:40 +0530

    [Sairam]Added:Ability to add the contacts into address book

--- AdvBook/Book.sql
+++ AdvBook/Book.sql
@@ -11,3 +11,10 @@ zipcode int,
 phone_number varchar(255),
 email_id varchar(255),
 );
+insert into Address_Book
+values('Ranjith','Kumar','Nehuru street','Mumbai','Maharastra','400768','7546637878','ranjith@gmail.com'),
+('Mohan','Reddy','Balaji Street','Vizag','Andhrapradesh','500036','7644566278','mohan@gmail.com'),
+('Madhu','Reddy','abc Street','Vizayawada','Andhrapradesh','500056','7644565778','madhu@gmail.com'),
+('John','Wick','def Street','Banglore','Karnataka','500066','7645676278','john@gmail.com'),
+('Martin','Guptill','hij Street','Indore','Punjab','505536','7644564568','martin@gmail.com')
+select * from Address_Book
